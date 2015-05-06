6e4b23398941e6853367a5b93824d66ea4ded2e2
Tuhin Bepari <digitaldreams40@gmail.com>
2015-05-07 02:36:44 +0600

    Working with template engine

--- template.js
+++ template.js
@@ -1,6 +1,7 @@
 function Template() {
     this.tempHtml = '';
     this.placeHolders;
+    this.filter = {};
     this.data;
     this.ajax;
     this.elem;
@@ -16,6 +17,8 @@ Template.prototype = {
                 this[prop] = setting[prop];
             }
         }
+        this.elem = document.querySelector(this.tempId).innerHTML;
+        this.parse(this.data);
     },
     collectData: function () {
         if (typeof this.ajax != "undefined") {
@@ -24,31 +27,60 @@ Template.prototype = {
             }
         }
     },
-    parse: function () {
-        this.elem = document.querySelector(this.tempId).innerHTML;
-        for (var i = 0; i < this.data.length; i++) {
+    parse: function (sourceData) {
+        this.tempHtml = "";
+        for (var i = 0; i < sourceData.length; i++) {
             var singleItem = this.elem;
-            var row = this.data[i];
+            var row = sourceData[i];
             var parentThis = this;
             var pureSingleItem = singleItem.replace(/\{\{(\w*)\}\}/g, function (a, b) {
                 var objectKey = b;
                 if (typeof parentThis.placeHolders != "undefined") {
-                    if (typeof parentThis.placeHolders[b] != 'undefined') {
+                    if (parentThis.placeHolders.hasOwnProperty(b)) {
                         objectKey = parentThis.placeHolders[b];
                     } else {
                         objectKey = b;
                     }
                 }
-                if (typeof row[objectKey] != "undefined") {
+                if (row.hasOwnProperty(objectKey)) {
                     return row[objectKey]
-                    console.log(b);
                 } else {
-                  //  console.log(b);
+                    //  console.log(b);
                 }
             });
             this.tempHtml += pureSingleItem;
         }
         document.querySelector(this.tempId).innerHTML = this.tempHtml;
+
+    },
+    formValue: function (formName) {
+        var parentThis = this;
+        var formELem = document.forms[formName].elements;
+        document.forms[formName].addEventListener('submit', function (ev) {
+            ev.preventDefault();
+            var elementsName = Object.keys(this.elements);
+            for (var f = 0; f < elementsName.length; f++) {
+                if (parentThis.placeHolders.hasOwnProperty(elementsName[f])) {
+                    var currentELem = parentThis.placeHolders[elementsName[f]];
+                    // console.log(currentELem);
+                    parentThis.filter[currentELem] = this.elements[elementsName[f]].value;
+                }
+            }
+            var tempSourceData = parentThis.data;
+            for (var prop in parentThis.filter) {
+                if (parentThis.filter.hasOwnProperty(prop)) {
+                    tempSourceData = tempSourceData.filter(function (value) {
+                        var rex = new RegExp('.*' + parentThis.filter[prop].trim() + '.*', 'i');
+                        if (value.hasOwnProperty(prop)) {
+                            return value[prop].match(rex);
+                        }
+
+                    });
+                }
+            }
+            document.querySelector(parentThis.tempId).innerHTML = " ";
+            parentThis.parse(tempSourceData);
+        });
 
     }
 }
